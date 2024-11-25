1d626ce5b50f6fc78a1acb929a920266b570817b
MatthewHawkins <matthew.hawkins1115@gmail.com>
2024-11-25 10:31:15 +0100

    feat: Adding release blog for 24.20

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,9 +49,9 @@ const config = {
       contextualSearch: false,
     },
     announcementBar: {
-      id: '24.12',
+      id: '24.20',
       content:
-        'We are excited to announce webforJ version 24.12 is live! Read more about the changes and features <a href=/blog/whats-new-v24.12>here.</a> ',
+        'We are excited to announce webforJ version 24.20 is live! Read more about the changes and features <a href=/blog/whats-new-v24.20>here.</a> ',
       isCloseable: true,
     },
     image: '/img/webforj.svg',
